e7da20f14bc54f788c6693200c720c77d91c6362
mingyueanyao <1981167302@qq.com>
2017-11-16 01:53:06 +0800

    实现课程按钮对后台数据的更新

--- Bachelor_Road/app/src/main/java/com/example/kobe/bachelor_road/Login.java
+++ Bachelor_Road/app/src/main/java/com/example/kobe/bachelor_road/Login.java
@@ -39,8 +39,7 @@ public class Login extends AppCompatActivity {
                 Toast.makeText(Login.this,"您选择为男生",
                         Toast.LENGTH_SHORT).show();
             }
-        }
-        );
+        });
         buttonHeadGirl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,8 +47,7 @@ public class Login extends AppCompatActivity {
                 Toast.makeText(Login.this,"您选择为女生",
                         Toast.LENGTH_SHORT).show();
             }
-        }
-        );
+        });
 
 
         //获取登录界面的学生注册信息,点击确定按钮
@@ -76,7 +74,7 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "请选择正确的头像",
                             Toast.LENGTH_SHORT).show();
                     return;
-                } else if(isOk(stuName, stuNum)) {
+                } else if(isOk(stuName, stuNum)==true) {
                     //（待写入）存入数据库
                     Character character = new Character(1,stuName,stuNum,stuClass,boyOrGirl,100,100,0,0,480,1);
                     long results = databaseManage.insertCharacter(character);
@@ -88,16 +86,13 @@ public class Login extends AppCompatActivity {
                     finish();
                     Toast.makeText(Login.this,"注册成功",
                             Toast.LENGTH_SHORT).show();
-
-
                 }
             }
-        }
-        );
+        });
     }
 
     public boolean isOk(String name, String num){
-        if(name == null||num == null|| name.equals("")||num.equals("")) {
+        if(name == null||num == null|| name.equals("")==true||num.equals("")==true) {
             return false;
         } else {
             return true;
